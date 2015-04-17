02f754faa90ddf654499c7ac3b7c27139e1197c1
cmartens <cmartens@DAL-CMARTENS1.tmwsystems.com>
2015-04-17 15:03:09 -0500

    Initial commit

---
+++ rap-hello/net.martenscs.wso2.application.hello/src/net/martenscs/wso2/application/hello/Activator.java
@@ -0,0 +1,14 @@
+package net.martenscs.wso2.application.hello;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator {
+
+    public void start(BundleContext context) throws Exception {
+    }
+
+    public void stop(BundleContext context) throws Exception {
+    }
+
+}
---
+++ rap-hello/net.martenscs.wso2.application.hello/src/net/martenscs/wso2/application/hello/BasicApplication.java
@@ -0,0 +1,19 @@
+package net.martenscs.wso2.application.hello;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.rap.rwt.application.Application;
+import org.eclipse.rap.rwt.application.ApplicationConfiguration;
+import org.eclipse.rap.rwt.client.WebClient;
+
+
+public class BasicApplication implements ApplicationConfiguration {
+
+    public void configure(Application application) {
+        Map<String, String> properties = new HashMap<String, String>();
+        properties.put(WebClient.PAGE_TITLE, "WSO2 Application");
+        application.addEntryPoint("/hello", BasicEntryPoint.class, properties);
+    }
+
+}
